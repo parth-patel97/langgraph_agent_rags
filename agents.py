eed3236e4324dae2b2651e13ef2ba12262767d6d
parth-patel97 <parthpatel6037@gmail.com>
2024-09-23 22:17:17 +0530

    add agent

--- agents.py
+++ agents.py
@@ -17,6 +17,17 @@ class AgentState(MessagesState):
 
 @tool
 def get_weather(city: Literal["ahm", "surat"]):
-    '''Use this to get weather information.'''
+    """Use this to get weather information."""
+    if city == "nyc":
+        return "It is cloudy in NYC, with 5 mph winds in the North-East direction and a temperature of 70 degrees"
+    elif city == "sf":
+        return "It is 75 degrees and sunny in SF, with 3 mph winds in the South-East direction"
+    else:
+        raise AssertionError("Unknown city")
     
-    return WeatherResponse()
+tools = [get_weather]
+    
+model = ChatAnthropic(model="claude-3-opus-20240229")
+    
+model_with_tools = model.bind_tools(tools)
+model_with_structured_output = model.with_structured_output(WeatherResponse)
